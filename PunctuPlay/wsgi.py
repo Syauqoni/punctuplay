a8323aae79ef1288f0f5e59d7d364e6855ceea67
Syauqoni <sitiqoni555@gmail.com>
2025-10-23 19:29:36 +0800

    Commit Pertama - Upload Projek Django

---
+++ PunctuPlay/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for PunctuPlay project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'PunctuPlay.settings')
+
+application = get_wsgi_application()
---
+++ blog/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def login(request):
+    return render(request, 'blog/login.html')
+
+def register(request):
+    return render(request, 'blog/register.html')
