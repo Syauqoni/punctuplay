2a120ec2d2cbdb61686022c034cabe6ecceaf9ad
Syauqoni <sitiqoni555@gmail.com>
2025-12-19 14:21:04 +0800

    Fix project name for Gunicorn

--- dump_utf8.py
+++
@@ -1,9 +0,0 @@
-import json
-from django.core.management import call_command
-
-with open("data.json", "w", encoding="utf-8") as f:
-    call_command(
-        "dumpdata",
-        exclude=["auth.permission", "contenttypes"],
-        stdout=f
-    )
